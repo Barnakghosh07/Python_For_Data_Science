0ab86837088cf9b36e6aeca4bfcc1780d6589844
Barnak Ghosh <101245955+Barnakghosh07@users.noreply.github.com>
2024-02-11 09:36:38 +0530

    Add files via upload
    Basics of file handling in python. Read and Write methods in Python!

---
+++ 11_file_handling.py
@@ -0,0 +1,24 @@
+# f = open("funny.txt", "r")
+
+# for line in f:
+#     print(line)
+
+# f.close()
+
+
+# with open("funny.txt", "r") as f:
+#     for line in f:
+#         print(line)
+
+
+# with open("love.txt", "a") as f:
+#     f.write("\nI love scala")
+
+
+# with open("love.txt", "w") as f:
+#     f.writelines([
+#         "I love Biriyani\n",
+#         "I love Chinese Food\n", 
+#         "I love Tibetian Food"
+#     ])
+
